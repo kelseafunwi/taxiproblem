bfc091cbf5c10e621fc396c5a0c7443dc36a9e3c
Funwi Kelsea <funwikelseandohnwi@gmail.com>
2023-11-15 14:21:57 +0100

    deleted again

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { NavBar } from './Components/NavBar/NavBar'
 
 export default function Root() {
   return (
     <>
-      <NavBar />
       <div className="body">
           <Outlet />
       </div>
