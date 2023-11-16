d86d6d5cde05efa2727c4a3ecc753222a073eea8
Funwi Kelsea <funwikelseandohnwi@gmail.com>
2023-11-16 12:40:06 +0100

    corrected my error

--- src/Components/OurServices.jsx
+++ src/Components/OurServices.jsx
@@ -1,37 +1,38 @@
-// import { motion } from 'framer-motion';
-// import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
 
-// const services = [
-//   '24/7 Availability',
-//   'Airport Transfer',
-//   'Long Distance Travel',
-//   'Local City Travel',
-//   'Online Booking',
-//   'Luxury Vehicles',
-//   'Professional Drivers',
-//   'Competitive Rates'
-// ];
+const services = [
+  '24/7 Availability',
+  'Airport Transfer',
+  'Long Distance Travel',
+  'Local City Travel',
+  'Online Booking',
+  'Luxury Vehicles',
+  'Professional Drivers',
+  'Competitive Rates'
+];
 
-// ServiceCard.propTypes = {
-//     service: PropTypes.string.isRequired,
-// };
 
-// const ServiceCard = ({ service }) => (
-//   <motion.div 
-//     className="bg-yellow-500 p-4 rounded-lg shadow-md m-2"
-//     whileHover={{ scale: 1.1 }}
-//     whileTap={{ scale: 0.9 }}
-//   >
-//     <h2 className="text-white text-lg font-bold">{service}</h2>
-//   </motion.div>
-// );
+const ServiceCard = ({ service }) => (
+  <motion.div 
+    className="bg-yellow-500 p-4 rounded-lg shadow-md m-2"
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+  >
+    <h2 className="text-white text-lg font-bold">{service}</h2>
+  </motion.div>
+);
 
-// const OurServices = () => (
-//   <div className="flex flex-wrap justify-center p-4">
-//     {services.map((service, index) => (
-//       <ServiceCard key={index} service={service} />
-//     ))}
-//   </div>
-// );
+ServiceCard.propTypes = {
+    service: PropTypes.string.isRequired,
+};
 
-// export default OurServices;
+const OurServices = () => (
+  <div className="flex flex-wrap justify-center p-4">
+    {services.map((service, index) => (
+      <ServiceCard key={index} service={service} />
+    ))}
+  </div>
+);
+
+export default OurServices;
